b2d70b46f7670857a4fa120178d2145ac36482a4
Romangonzalezz <romanynaarut@gmail.com>
2023-09-06 21:38:53 -0300

    Carpetas y archivos clase viernes

--- models/server.js
+++ models/server.js
@@ -4,23 +4,37 @@ const express = require('express');
 class Server {
     
     constructor(){
-        this.port = process.env.PORT || 3000;
 
+        this.port = process.env.PORT || 3000;
         this.app = express();
 
+        this.middleware();
         this.routers();
+
     }   
 
+    middleware(){
+
+        this.app.use(cors());
+        this.app.use(express.static('public'));
+        
+    }
+
     routers(){
+
         this.app.get('/', function (req, res) {
             res.send('Hello World')
         });   
     }
 
+
+
     listen(){
+
         this.app.listen(this.port, () =>{
             console.log(`App escuchando el el puerto ${this.port}`);
         });
+
     }
 
 
